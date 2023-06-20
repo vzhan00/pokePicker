c2f601eebadf32b009cf2c811f6cd627b9a2f4f5
ike <ikekaper@gmail.com>
2023-06-20 13:57:12 -0400

    Made edit UI a bit better

--- pokePicker/pokePicker/Pokemon/EditViewController.swift
+++ pokePicker/pokePicker/Pokemon/EditViewController.swift
@@ -32,55 +32,65 @@ class EditViewController: UIViewController {
     
     func setupUI() {
         view.backgroundColor = .white
-        
+
         nameTextField = UITextField()
         nameTextField.placeholder = "Name"
         nameTextField.borderStyle = .roundedRect
         nameTextField.font = UIFont.systemFont(ofSize: 14)
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nameTextField)
-        
+
         move1TextField = UITextField()
         move1TextField.placeholder = "Move 1"
         move1TextField.borderStyle = .roundedRect
         move1TextField.font = UIFont.systemFont(ofSize: 14)
         move1TextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(move1TextField)
-        
+
         move2TextField = UITextField()
         move2TextField.placeholder = "Move 2"
         move2TextField.borderStyle = .roundedRect
         move2TextField.font = UIFont.systemFont(ofSize: 14)
         move2TextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(move2TextField)
-        
+
         move3TextField = UITextField()
         move3TextField.placeholder = "Move 3"
         move3TextField.borderStyle = .roundedRect
         move3TextField.font = UIFont.systemFont(ofSize: 14)
         move3TextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(move3TextField)
-        
+
         move4TextField = UITextField()
         move4TextField.placeholder = "Move 4"
         move4TextField.borderStyle = .roundedRect
         move4TextField.font = UIFont.systemFont(ofSize: 14)
         move4TextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(move4TextField)
-        
+
         NSLayoutConstraint.activate([
-            nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             nameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            move1TextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            move1TextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             move1TextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 16),
-            move2TextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            move1TextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            move2TextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             move2TextField.topAnchor.constraint(equalTo: move1TextField.bottomAnchor, constant: 16),
-            move3TextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            move2TextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            move3TextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             move3TextField.topAnchor.constraint(equalTo: move2TextField.bottomAnchor, constant: 16),
-            move4TextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            move4TextField.topAnchor.constraint(equalTo: move3TextField.bottomAnchor, constant: 16)
+            move3TextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            move4TextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            move4TextField.topAnchor.constraint(equalTo: move3TextField.bottomAnchor, constant: 16),
+            move4TextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
     }
+
     
     func populateFields() {
         guard let pokemon = pokemon else { return }
